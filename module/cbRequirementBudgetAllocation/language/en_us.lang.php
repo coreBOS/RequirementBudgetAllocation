561849f4f97ee935829d2851e6fd0d5d693cd54a
glorymoshi <glorysimon95@gmail.com>
2021-04-23 12:53:09 +0300

    feat(module)create cbRequirementBudgetAllocation module

---
+++ module/cbRequirementBudgetAllocation/language/en_us.lang.php
@@ -0,0 +1,27 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = array(
+	'cbRequirementBudgetAllocation' => 'Requirement-Budget Allocations',
+	'SINGLE_cbRequirementBudgetAllocation' => 'Requirement-Budget Allocation',
+	'cbRequirementBudgetAllocation ID' => 'RBA ID',
+
+	'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+	'LBL_cbRequirementBudgetAllocation_INFORMATION' => 'Requirement-Budget Allocation',
+	'LBL_DESCRIPTION_INFORMATION' => 'Description',
+
+
+	'Requirement Budget Allocation No' => 'Requirement-BA No',
+	'Requirement' => 'Requirement',
+	'Budget Item' => 'Budget Item',
+	'Percentage' => 'Percentage',
+	'Allocated Amount' => 'Allocated Amount',
+);
+?>
